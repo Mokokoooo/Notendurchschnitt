b85d438fdc3bca21eef33d737ceb5780efc0e245
Mokokoooo <131966777+Mokokoooo@users.noreply.github.com>
2023-11-22 15:37:01 +0000

    Ersten Schritte

---
+++ hello/src/main/java/NotenController/NotenController.java
@@ -0,0 +1,17 @@
+package NotenController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class NotenController {
+    
+    @RequestMapping(value="hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String getHelloWorld(){
+        return "<h1>Hello World Spring Boot !</h1>";
+    }
+}
